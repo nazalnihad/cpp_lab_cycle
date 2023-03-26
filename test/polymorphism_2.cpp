34890e356f3ed3d5ce265d7ea988e4f0a7e5aff8
Nazal-nihad <nazalnihad17@gmail.com>
2023-03-27 01:06:45 +0530

    polymorphism

---
+++ test/polymorphism_2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+class poly
+{
+    char *name;
+    int len;
+
+    public:
+    poly()
+    {
+        len = 0;
+        name = new char[len+1];
+    }
+    void get_name();
+    void display()
+    {
+        std::cout<<"The name of the city is : "<<name<<"\n";
+    }
+};
+
+void poly::get_name()
+{
+    char s[10];
+    std::cout<<"Enter the city name : ";
+    std::cin>>s;
+
+    len = strlen(s);
+    // delete name;
+    name = new char[len+1];
+    strcpy(name,s);
+}
+
+int main()
+{
+    poly *ptr[5];
+
+    for(int i=0;i<5;i++)
+    {
+        ptr[i] = new poly;
+        ptr[i]->get_name();
+    }
+
+    for(int i=0;i<5;i++)
+    {
+        ptr[i]->display();
+    }
+return 0;
+}
