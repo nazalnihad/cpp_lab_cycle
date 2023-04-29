dd81dd3e2a2f27bf45426f81fdd2e9f13de95444
Nazal-nihad <nazalnihad17@gmail.com>
2023-04-29 23:16:25 +0530

    some more changes in cycle 2

--- lab_cycle_2/QN4.cpp
+++ lab_cycle_2/QN4.cpp
@@ -1,25 +1,18 @@
-#include <iostream> 
+#include <iostream>
 class matrix
 {
     int **p;
-    int rows,cols;
+    int rows, cols;
 
-    public:
-    matrix(){}
-    matrix(int,int);
-    void getelement(int,int,int);
-    int putelement(int,int);
+public:
+    matrix() {}
+    matrix(int, int);
+    void getelement(int, int, int);
+    int putelement(int, int);
     matrix getmatrix();
+    matrix operator+(matrix &);
+    matrix operator*(matrix &);
     void display();
-    matrix operator+(matrix&);
-    matrix operator*(matrix&);
-    // ~matrix(){
-    // for(int i = 0; i < rows; i++){
-    //     delete[] p[i];
-    // }
-    // std::cout<<"MM\n";
-    // delete[] p;
-// }
 };
 matrix::matrix(int x, int y)
 {
@@ -29,105 +22,146 @@ matrix::matrix(int x, int y)
     for (int i = 0; i < rows; i++)
     {
         p[i] = new int[cols];
-    }   
+    }
 }
-void matrix::getelement(int x,int y, int value)
+void matrix::getelement(int x, int y, int value)
 {
     p[x][y] = value;
 }
-int matrix::putelement(int x,int y)
+int matrix::putelement(int x, int y)
 {
     return p[x][y];
 }
 matrix matrix::getmatrix()
 {
-    int value,get_rows,get_cols;
-    std::cout<<"Enter no of rows : ";
-    std::cin>>get_rows;
-    std::cout<<"Enter no of cols : ";
-    std::cin>>get_cols;
-    std::cout<<get_cols<<"\n"<<get_rows<<"\n";
-    matrix a(get_rows,get_cols);
-    
-    for(int i=0;i<get_rows;i++)
+    int value;
+
+    for (int i = 0; i < rows; i++)
     {
-        std::cout<<"ROW "<<i+1<<"\n";
-        for(int j=0;j<get_cols;j++)
+        std::cout << "ROW " << i + 1 << "\n";
+        for (int j = 0; j < cols; j++)
         {
-            std::cout<<"Enter value at ["<<i<<"]["<<j<<"] : ";
-            std::cin>>value;
-            a.getelement(i,j,value);
+            std::cout << "Enter value at [" << i << "][" << j << "] : ";
+            std::cin >> value;
+            getelement(i, j, value);
         }
     }
-    return a;
+    // return a;
 }
 void matrix::display()
-{   
-    std::cout<<"\n========\n";
-    for(int i=0; i<rows;i++)
+{
+    std::cout << "\n========\n";
+    for (int i = 0; i < rows; i++)
     {
-        for(int j=0;j<cols;j++)
+        for (int j = 0; j < cols; j++)
         {
-            std::cout<<putelement(i,j)<<" ";
+            std::cout << putelement(i, j) << " ";
         }
-        std::cout<<"\n";
+        std::cout << "\n";
     }
-    std::cout<<"========\n";
+    std::cout << "========\n";
 }
-matrix matrix::operator +(matrix&a)
+matrix matrix::operator+(matrix &a)
 {
     int value;
-    if(a.rows==rows && a.cols == cols)
+
+    matrix add(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++)
     {
-        matrix add(a.rows,a.cols);
-        for(int i=0;i<a.rows;i++)
+        for (int j = 0; j < a.cols; j++)
         {
-            for(int j=0;j<a.cols;j++)
-            {
-                value = a.putelement(i,j)+putelement(i,j);
-                add.getelement(i,j,value);
-            }
+            value = a.putelement(i, j) + putelement(i, j);
+            add.getelement(i, j, value);
         }
+    }
     return add;
-    }
-    else
-    {
-        std::cout<<"ROWS and COlS must match \n";
-    }
 }
 matrix matrix::operator*(matrix &a)
 {
-    if(cols == a.rows)
+
+    matrix mult(rows, a.cols);
+    for (int i = 0; i < rows; i++)
     {
-        matrix mult(rows,a.cols);
-        for(int i=0;i<rows;i++)
+        for (int j = 0; j < a.cols; j++)
         {
-            for(int j=0;j<a.cols;j++)
+            int value = 0;
+            for (int k = 0; k < a.rows; k++)
             {
-                int value=0;
-                for(int k=0;k<a.rows;k++)
-                {
-                    value = value+putelement(i,k)*a.putelement(k,j);
-                    mult.getelement(i,j,value);
-                }
+                value = value + putelement(i, k) * a.putelement(k, j);
+                mult.getelement(i, j, value);
             }
         }
+    }
     return mult;
-    }
-    else{
-        std::cout<<"NO can do \n";
-    }
 }
 int main()
 {
-    matrix a,b,sum,pdt;
-    a=a.getmatrix();
-    b=b.getmatrix();
-    sum=a+b;
-    pdt = a*b;
-    a.display();
-    b.display();
-    sum.display();
-    pdt.display();
+    matrix sum, pdt;
+    int choice, ask;
+    int r1, r2, c1, c2;
+    do
+    {
+        std::cout << "\nEnter the first matrix \n";
+        std::cout << "enter no of rows : ";
+        std::cin >> r1;
+        std::cout << "enter no of cols : ";
+        std::cin >> c1;
+        matrix a(r1, c1);
+        a.getmatrix();
+
+        std::cout << "\nEnter the second matrix \n";
+        std::cout << "enter no of rows : ";
+        std::cin >> r2;
+        std::cout << "enter no of cols : ";
+        std::cin >> c2;
+        matrix b(r2, c2);
+        b.getmatrix();
+        do
+        {
+            std::cout << "\n1 - to display the matrices \n2 - to add the matrices \n3 - to multiply matrices \n4 - to exit \n : ";
+            std::cin >> choice;
+            switch (choice)
+            {
+            case 1:
+                std::cout << "\nThe entered matrices are\n";
+                a.display();
+                b.display();
+                break;
+
+            case 2:
+                if (r1 == r2 && c1 == c2)
+                {
+                    sum = a + b;
+                    sum.display();
+                }
+                else
+                {
+                    std::cout << "\nrows and cols must be equal for addition\n";
+                }
+                break;
+
+            case 3:
+                if (c1 == r2)
+                {
+                    pdt = a * b;
+                    pdt.display();
+                }
+                else
+                {
+                    std::cout << "\ncols of first matrix must be equal to rows of second matrix\n";
+                }
+                break;
+
+            case 4:
+                std::cout << "\n1 - to continue with different values \nany other key to quit \n : ";
+                std::cin >> ask;
+                continue;
+
+            default:
+                std::cout << "\nEnter a valid value ";
+            }
+        } while (choice != 4);
+    } while (ask == 1);
+    std::cout << "\nExiting...";
     return 0;
 }
--- lab_cycle_2/QN6.cpp
+++ lab_cycle_2/QN6.cpp
@@ -1,104 +1,103 @@
 #include <iostream>
 
-class student //base class
+class student
 {
-    protected:
+protected:
     int roll_no;
     std::string name;
 
-    public:
+public:
     void getDetails();
     void displayDetails();
 };
 void student::getDetails()
 {
-    std::cout<<"Enter your name : ";
-    std::cin>>name;
-    std::cout<<"Enter the Roll number : ";
-    std::cin>>roll_no;
+    std::cout << "Enter your name : ";
+    std::cin >> name;
+    std::cout << "Enter the Roll number : ";
+    std::cin >> roll_no;
 }
 void student::displayDetails()
 {
-    std::cout<<"\n=== DETAILS OF THE STUDENT ===\n";
-    std::cout<<"NAME : "<<name<<"\n";
-    std::cout<<"ROLL NUMBER : "<<roll_no<<"\n";
+    std::cout << "\n=== DETAILS OF THE STUDENT ===\n";
+    std::cout << "NAME : " << name << "\n";
+    std::cout << "ROLL NUMBER : " << roll_no << "\n";
 }
 
-
-class test:public student //derived class
+class test : virtual public student
 {
-    protected:
+protected:
     int subject_marks[3];
 
-    public:
+public:
     void getMarks();
     void displayMarks();
 };
 void test::getMarks()
 {
-    for(int i=0;i<3;i++)
+    for (int i = 0; i < 3; i++)
     {
-        std::cout<<"Enter marks in Subject "<<i+1<<" : ";
-        std::cin>>subject_marks[i];
+        std::cout << "Enter marks in Subject " << i + 1 << " : ";
+        std::cin >> subject_marks[i];
     }
 }
 void test::displayMarks()
 {
-    std::cout<<"\n===== SCORES ===== \n";
-    for(int i=0;i<3;i++)
+    std::cout << "\n===== SCORES ===== \n";
+    for (int i = 0; i < 3; i++)
     {
-        std::cout<<"Score of subject "<<i+1<<" is : "<<subject_marks[i]<<"\n"; 
+        std::cout << "Score of subject " << i + 1 << " is : " << subject_marks[i] << "\n";
     }
 }
 
-class sports //base class 2
+class sports : virtual public student
 {
-    protected:
-    int football,cricket;
+protected:
+    int football, cricket;
 
-    public:
+public:
     void getScores();
     void displayScores();
 };
 void sports::getScores()
 {
-    std::cout<<"Enter the sports score in football : ";
-    std::cin>>football;
-    std::cout<<"Enter the sports score in cricket : ";
-    std::cin>>cricket;
+    std::cout << "Enter the sports score in football : ";
+    std::cin >> football;
+    std::cout << "Enter the sports score in cricket : ";
+    std::cin >> cricket;
 }
 void sports::displayScores()
 {
-    std::cout<<"Football Score : "<<football<<"\n";
-    std::cout<<"Cricket Score : "<<cricket<<"\n";
+    std::cout << "Football Score : " << football << "\n";
+    std::cout << "Cricket Score : " << cricket << "\n";
 }
 
-class result :public test,public sports //derived class through hybrid inheritence from test(i.e student is base) and score(separate class)
+class result : public test, public sports
 {
-    int total_sub,total_sports;
-    public:
-    result(){}
+    int total_sub, total_sports;
+
+public:
+    result() {}
     int addScores();
     void display();
 };
 int result::addScores()
 {
     total_sub = 0;
-    for(int i=0;i<3;i++)
+    for (int i = 0; i < 3; i++)
     {
-        total_sub = total_sub+subject_marks[i];
+        total_sub = total_sub + subject_marks[i];
     }
-    total_sports = football+cricket;
-    return total_sub,total_sports;
+    total_sports = football + cricket;
 }
 void result::display()
 {
     displayDetails();
     displayMarks();
     displayScores();
-    std::cout<<"\n====== TOTAL ======\n";
-    std::cout<<"Total subject scores : "<<total_sub<<"\n";
-    std::cout<<"Total sports scores : "<<total_sports;
+    std::cout << "\n====== TOTAL ======\n";
+    std::cout << "Total subject scores : " << total_sub << "\n";
+    std::cout << "Total sports scores : " << total_sports;
 }
 
 int main()
