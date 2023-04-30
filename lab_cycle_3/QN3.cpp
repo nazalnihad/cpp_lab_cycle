5c30dd234c043df7b4ec7a9c8a031efe06c52879
Nazal-nihad <nazalnihad17@gmail.com>
2023-04-30 21:41:01 +0530

    final changes for latex ig , until now

--- lab_cycle_3/QN3.cpp
+++ lab_cycle_3/QN3.cpp
@@ -5,27 +5,38 @@ float zeroDivision(float x, float y)
     {
         throw "Zero division error !";
     }
-    return x / y;
+    else
+    {
+        return x / y;
+    }
 }
 int main()
 {
     float x, y;
-    std::cout << "Enter X in x/y : ";
-    std::cin >> x;
-    while (true)
+    int choice;
+    std::cout << "\n1 - to start \nany other key to quit \n : ";
+    std::cin >> choice;
+    do
     {
+        std::cout << "\nEnter X in x/y : ";
+        std::cin >> x;
+
         std::cout << "\nEnter Y in x/y : ";
         std::cin >> y;
         try
         {
             float result = zeroDivision(x, y);
-            std::cout << "Result = " << result << "\n";
-            break;
+            std::cout << "\nResult = " << result << "\n";
         }
         catch (const char *s)
         {
             std::cout << s << '\n';
         }
-    }
+
+        std::cout << "\n1 - continue \nany other key to quit \n : ";
+        std::cin >> choice;
+
+    } while (choice == 1);
+    std::cout << "\nExiting..";
     return 0;
 }
